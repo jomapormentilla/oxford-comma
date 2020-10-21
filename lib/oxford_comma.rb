a19e3ac7baa5fee9eb6c82dacf592dc6ef4a4a22
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 12:46:25 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+    if( array.length <= 2 )
+        array.join(" and ")
+    else
+        last = array.pop
+        last_el = ", and #{ last }"
 
+        string = array.join(", ")
+        string += last_el
+
+        string
+    end
 end
